305f168a709071eedc79203e3899b7ff1834cf1e
AGA TOMOHIRO <60427250+agachan@users.noreply.github.com>
2020-09-26 11:38:46 +0900

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,158 +1,158 @@
 const a = [{
     "type":"Rice0",
     "menu":"マイルドカレー",
-    "rarity":"",
-    "price": ""
+    "rarity":"R",
+    "price":300
 },
            {
     "type":"Rice0",
     "menu":"カレーライス",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":300
 },
            {
     "type":"Rice0",
     "menu":"塩豚カルビ丼",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":400
 },
            {
     "type":"Rice0",
     "menu":"炭火焼き鳥丼",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":400
 },
            {
     "type":"Rice0",
     "menu":"照り焼きチキンカツ丼",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":400
 },
            {
     "type":"Rice0",
     "menu":"牛丼",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":400
 },
            {
     "type":"Rice0",
     "menu":"洋食屋さんのハヤシライス",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":400
 },
            {
     "type":"Rice0",
     "menu":"チキンカツ卵とじ丼",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":400
 },
            {
     "type":"Rice0",
     "menu":"麻婆丼",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":400
 },
            {
     "type":"Rice0",
     "menu":"肉そば",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":400
 },
            {
     "type":"Rice0",
     "menu":"肉うどん",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":300
 },           {
     "type":"Rice0",
     "menu":"かけそば",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":200
 },           {
     "type":"Rice0",
     "menu":"かけうどん",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":200
 },           {
     "type":"Rice0",
     "menu":"若布そば",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":200
 },           {
     "type":"Rice0",
     "menu":"若布うどん",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":200
 },           {
     "type":"Rice0",
     "menu":"きつねそば",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":300
 },           {
     "type":"Rice0",
     "menu":"天ぷらそば",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":200
 },           {
     "type":"Rice0",
     "menu":"天ぷらうどん",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":200
 },           {
     "type":"Rice0",
     "menu":"醤油ラーメン",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":200
 },           {
     "type":"Rice0",
     "menu":"豚骨ラーメン",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":400
 },           {
     "type":"Rice0",
     "menu":"坦々麺",
-    "rarity":"",
-    "price":,
+    "rarity":"SR",
+    "price":300
 },           {
     "type":"Rice0",
     "menu":"冷やしそば",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":300
 },           {
     "type":"Rice0",
     "menu":"冷やしうどん",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":300
 },           {
     "type":"Rice0",
     "menu":"ぶっかけおろし冷うどん",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":300
 },           {
     "type":"Rice0",
     "menu":"温玉ぶっかけ冷うどん",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":300
 },           {
     "type":"Rice0",
     "menu":"冷やし中華",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":400
 },           {
     "type":"Rice0",
     "menu":"冷やし坦々麺",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":400
 },           {
     "type":"Rice0",
     "menu":"黒ごま冷やし坦々麺",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":400
 },           {
     "type":"Rice0",
     "menu":"チキンと野菜のごまだれ冷麺",
     "rarity":"SR",
-    "price":,
+    "price":500
 },
            
            
@@ -161,124 +161,79 @@ const a = [{
     "type":"Side",
     "menu":"豆腐",
     "rarity":"",
-    "price":,
+    "price":100
 }, 
            {
     "type":"Side",
     "menu":"納豆",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":80
 },           {
     "type":"Side",
     "menu":"エッグマカロニサラダ",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":80
 },           {
     "type":"Side",
     "menu":"法蓮草",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":80
 },           {
     "type":"Side",
     "menu":"かぼちゃ煮",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":80
 },           {
     "type":"Side",
     "menu":"たまご焼き",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":80
 },           {
     "type":"Side",
     "menu":"大学芋",
-    "rarity":"",
-    "price":,
+    "rarity":"R",
+    "price":80
 },           {
     "type":"Side",
     "menu":"おくら巣篭り卵",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":80
 },           {
     "type":"Side",
     "menu":"巣ごもり卵",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":
 },           {
     "type":"Side",
     "menu":"夏野菜と昆布の和物",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":80
 },           {
     "type":"Side",
     "menu":"小松菜と上げの煮浸し",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":100
 },           {
     "type":"Side",
     "menu":"ごろごろ野菜炊き合わせ",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":80
 },           {
     "type":"Side",
     "menu":"青菜と切り干し大根の煮物",
-    "rarity":"",
-    "price":,
+    "rarity":"N",
+    "price":80
 },          {
     "type":"Side",
     "menu":"",
     "rarity":"",
-    "price":,
+    "price":80
 },          {
     "type":"Side",
     "menu":"",
     "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},          {
-    "type":"Side",
-    "menu":"",
-    "rarity":"",
-    "price":,
-},     
+    "price":100
+},         
            
            
            
@@ -290,37 +245,37 @@ const a = [{
     "type":"LimitedTime-Rice0",
     "menu":"サーモンユッケ丼",
     "rarity":"SSR",
-    "price":528,
+    "price":528
 },
 　　　　　　　{
     "type":"LimitedTime-Rice0",
     "menu":"冷やし明太うどん",
     "rarity":"SR",
-    "price":451,
+    "price":451
 },
            {
     "type":"LimitedTime-Rice0",
     "menu":"炭火焼き鶏ハラミ丼",
     "rarity":"SSR",
-    "price":473,
+    "price":473
 },
            {
     "type":"LimitedTime-Rice0",
     "menu":"キムチ冷麺",
     "rarity":"SR",
-    "price":,
+    "price":
 },
           {
     "type":"LimitedTime-Rice0",
     "menu":"博多ラーメン",
     "rarity":"SR",
-    "price":473,
+    "price":473
 },
           {
     "type":"LimitedTime-Rice0",
     "menu":"焼肉ビビンバ丼",
     "rarity":"SSR",
-    "price":506,
+    "price":506
 },
            
            
@@ -333,37 +288,37 @@ const a = [{
     "type":"LimitedTime-Rice1",
     "menu":"ブラジル風チキン唐揚げ",
     "rarity":"R",
-    "price":308,
+    "price":308
 },
           {
     "type":"LimitedTime-Rice1",
     "menu":"こくうまキムチチキン",
     "rarity":"R",
-    "price":330,
+    "price":330
 },
           {
     "type":"LimitedTime-Rice1",
     "menu":"えびかつオーロラソース",
     "rarity":"R",
-    "price":352,
+    "price":352
 },
           {
     "type":"LimitedTime-Rice1",
     "menu":"トントロ塩だれ炒め",
     "rarity":"R",
-    "price":330,
+    "price":330
 },
           {
     "type":"LimitedTime-Rice1",
     "menu":"鶏天柚子タルタル",
     "rarity":"SR",
-    "price":308,
+    "price":308
 },
           {
     "type":"LimitedTime-Rice1",
     "menu":"牛肉とエリンギのハッシュドビーフ",
     "rarity":"SR",
-    "price":330,
+    "price":330
 }];
 
 export {a};
