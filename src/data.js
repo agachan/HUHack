830ebd5fb8e0d509b330289bac1d11e4735bf103
AGA TOMOHIRO <60427250+agachan@users.noreply.github.com>
2020-09-26 11:26:41 +0900

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,27 +1,369 @@
 const a = [{
-    "type":0
-    "menu":"カレー"
-    "size":1
-    "rarity":1
+    "type":"Rice0",
+    "menu":"マイルドカレー",
+    "rarity":"",
+    "price":,
 },
-{
-    "type":0
-    "menu":"うどん"
+           {
+    "type":"Rice0",
+    "menu":"カレーライス",
+    "rarity":"",
+    "price":,
 },
-{
-    "type":0
-    "menu":"ラーメン"
+           {
+    "type":"Rice0",
+    "menu":"塩豚カルビ丼",
+    "rarity":"",
+    "price":,
 },
-{
-    "type":0
-    "menu":"天丼"
+           {
+    "type":"Rice0",
+    "menu":"炭火焼き鳥丼",
+    "rarity":"",
+    "price":,
 },
-{
-    "type":0
-    "menu":""
+           {
+    "type":"Rice0",
+    "menu":"照り焼きチキンカツ丼",
+    "rarity":"",
+    "price":,
 },
-{
-    
+           {
+    "type":"Rice0",
+    "menu":"牛丼",
+    "rarity":"",
+    "price":,
+},
+           {
+    "type":"Rice0",
+    "menu":"洋食屋さんのハヤシライス",
+    "rarity":"",
+    "price":,
+},
+           {
+    "type":"Rice0",
+    "menu":"チキンカツ卵とじ丼",
+    "rarity":"",
+    "price":,
+},
+           {
+    "type":"Rice0",
+    "menu":"麻婆丼",
+    "rarity":"",
+    "price":,
+},
+           {
+    "type":"Rice0",
+    "menu":"肉そば",
+    "rarity":"",
+    "price":,
+},
+           {
+    "type":"Rice0",
+    "menu":"肉うどん",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"かけそば",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"かけうどん",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"若布そば",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"若布うどん",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"きつねそば",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"天ぷらそば",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"天ぷらうどん",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"醤油ラーメン",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"豚骨ラーメン",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"坦々麺",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"冷やしそば",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"冷やしうどん",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"ぶっかけおろし冷うどん",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"温玉ぶっかけ冷うどん",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"冷やし中華",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"冷やし坦々麺",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"黒ごま冷やし坦々麺",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Rice0",
+    "menu":"チキンと野菜のごまだれ冷麺",
+    "rarity":"SR",
+    "price":,
+},
+           
+           
+           
+           {
+    "type":"Side",
+    "menu":"豆腐",
+    "rarity":"",
+    "price":,
+}, 
+           {
+    "type":"Side",
+    "menu":"納豆",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"エッグマカロニサラダ",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"法蓮草",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"かぼちゃ煮",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"たまご焼き",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"大学芋",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"おくら巣篭り卵",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"巣ごもり卵",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"夏野菜と昆布の和物",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"小松菜と上げの煮浸し",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"ごろごろ野菜炊き合わせ",
+    "rarity":"",
+    "price":,
+},           {
+    "type":"Side",
+    "menu":"青菜と切り干し大根の煮物",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},          {
+    "type":"Side",
+    "menu":"",
+    "rarity":"",
+    "price":,
+},     
+           
+           
+           
+           
+           
+           
+           
+           {
+    "type":"LimitedTime-Rice0",
+    "menu":"サーモンユッケ丼",
+    "rarity":"SSR",
+    "price":528,
+},
+　　　　　　　{
+    "type":"LimitedTime-Rice0",
+    "menu":"冷やし明太うどん",
+    "rarity":"SR",
+    "price":451,
+},
+           {
+    "type":"LimitedTime-Rice0",
+    "menu":"炭火焼き鶏ハラミ丼",
+    "rarity":"SSR",
+    "price":473,
+},
+           {
+    "type":"LimitedTime-Rice0",
+    "menu":"キムチ冷麺",
+    "rarity":"SR",
+    "price":,
+},
+          {
+    "type":"LimitedTime-Rice0",
+    "menu":"博多ラーメン",
+    "rarity":"SR",
+    "price":473,
+},
+          {
+    "type":"LimitedTime-Rice0",
+    "menu":"焼肉ビビンバ丼",
+    "rarity":"SSR",
+    "price":506,
+},
+           
+           
+           
+           
+           
+           
+           
+          {
+    "type":"LimitedTime-Rice1",
+    "menu":"ブラジル風チキン唐揚げ",
+    "rarity":"R",
+    "price":308,
+},
+          {
+    "type":"LimitedTime-Rice1",
+    "menu":"こくうまキムチチキン",
+    "rarity":"R",
+    "price":330,
+},
+          {
+    "type":"LimitedTime-Rice1",
+    "menu":"えびかつオーロラソース",
+    "rarity":"R",
+    "price":352,
+},
+          {
+    "type":"LimitedTime-Rice1",
+    "menu":"トントロ塩だれ炒め",
+    "rarity":"R",
+    "price":330,
+},
+          {
+    "type":"LimitedTime-Rice1",
+    "menu":"鶏天柚子タルタル",
+    "rarity":"SR",
+    "price":308,
+},
+          {
+    "type":"LimitedTime-Rice1",
+    "menu":"牛肉とエリンギのハッシュドビーフ",
+    "rarity":"SR",
+    "price":330,
 }];
 
 export {a};
