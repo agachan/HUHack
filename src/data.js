31cdd5d61f59ca1afc30df21dee1326ee475af96
AGA TOMOHIRO <60427250+agachan@users.noreply.github.com>
2020-09-26 10:47:08 +0900

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,27 @@
 const a = {
-    "hoge": "piyo"
+    "type":0
+    "menu":"カレー"
+    "size":1
+    "rarity":1
+},
+{
+    "type":0
+    "menu":"うどん"
+},
+{
+    "type":0
+    "menu":"ラーメン"
+},
+{
+    "type":0
+    "menu":"天丼"
+},
+{
+    "type":0
+    "menu":""
+},
+{
+    
 };
 
 export {a};
